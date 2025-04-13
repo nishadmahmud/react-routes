2ee816d0ac781e607f2a0a1a33e4aaaf0f5c89ec
Nishad <mahmudnishad253@gmail.com>
2025-04-14 00:48:16 +0600

    routing examples

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import Mobiles from "./components/Mobiles/Mobiles.jsx";
+import Laptops from "./components/Laptops/Laptops.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {index:true,Component:Home},
+      {path:'mobiles', Component: Mobiles},
+      {path:'laptops', Component: Laptops}
+    ]
+  },
+  {
+    path: "about",
+    element: <div>About Us</div>,
+  },
+  {
+    path: "blogs",
+    element: <div>All blogs</div>,
+  },
+  {
+    path:'app',
+    Component: App,
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
